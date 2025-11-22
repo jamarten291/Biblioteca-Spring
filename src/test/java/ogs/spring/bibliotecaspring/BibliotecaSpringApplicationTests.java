0e925428b675a63ace1d08c1f70ba0a7c810661e
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2025-11-22 02:15:10 +0100

    first commit

---
+++ src/test/java/ogs/spring/bibliotecaspring/BibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ogs.spring.bibliotecaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
