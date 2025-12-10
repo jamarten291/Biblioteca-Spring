fa4fba80febe9765213bd9547c8f1c9de803152b
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2025-12-10 14:05:04 +0100

    finish V1

--- src/main/java/ogs/spring/bibliotecaspring/controller/PrestamoWebController.java
+++ src/main/java/ogs/spring/bibliotecaspring/controller/PrestamoWebController.java
@@ -1,7 +1,9 @@
 package ogs.spring.bibliotecaspring.controller;
 
 import jakarta.validation.Valid;
+import ogs.spring.bibliotecaspring.entity.Libro;
 import ogs.spring.bibliotecaspring.entity.Prestamo;
+import ogs.spring.bibliotecaspring.entity.Socio;
 import ogs.spring.bibliotecaspring.repository.LibroRepository;
 import ogs.spring.bibliotecaspring.repository.PrestamoRepository;
 import ogs.spring.bibliotecaspring.repository.SocioRepository;
@@ -10,7 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/prestamos")
+@RequestMapping()
 public class PrestamoWebController {
 
     private final LibroRepository libroRepository;
@@ -28,37 +30,43 @@ public class PrestamoWebController {
         return "menu";
     }
 
-    @GetMapping("/nuevo")
+    @GetMapping("/nuevo-prestamo")
     public String nuevoPrestamo(Model model) {
         // Agrega al modelo un préstamo vacío y una lista de socios y libros antes de devolver el formulario
         model.addAttribute("prestamo", new Prestamo());
-        model.addAttribute("socios", socioRepository.findAll());
-        model.addAttribute("libros", libroRepository.findAll());
-        return "crear-prestamo";
+        return "crear_prestamo";
     }
 
-    @GetMapping("/eliminar")
+    @GetMapping("/eliminar-prestamo")
     public String eliminarPrestamo(Model model) {
         model.addAttribute("prestamos", prestamoRepository.findAll());
-        return "eliminar-prestamo";
+        return "eliminar_prestamo";
     }
 
-    @GetMapping("/listar")
+    @GetMapping("/listar-prestamo")
     public String listadoPrestamo(Model model) {
         model.addAttribute("prestamos", prestamoRepository.findAll());
-        return "listado-prestamo";
+        return "listado_prestamo";
     }
 
     @PostMapping("/add-prestamo")
-    public String crearPrestamo(@Valid Prestamo prestamo) {
+    public String crearPrestamo(@Valid Prestamo prestamo,
+                                @RequestParam("socioId") Long socio,
+                                @RequestParam("libroId") Long libro) {
         // Guarda el préstamo recibido por el modelo
+        Socio socioObj = socioRepository.findById(socio).orElseThrow();
+        Libro libroObj = libroRepository.findById(libro).orElseThrow();
+
+        prestamo.setLibro(libroObj);
+        prestamo.setSocio(socioObj);
+
         prestamoRepository.save(prestamo);
-        return "redirect:/prestamos/listar";
+        return "redirect:/listar-prestamo";
     }
 
     @DeleteMapping("/delete-prestamo")
     public String borrarPrestamoByParam(@RequestParam("prestamoId") Long id) {
         prestamoRepository.deleteById(id);
-        return "redirect:/prestamos/listar";
+        return "redirect:/listar-prestamo";
     }
 }
